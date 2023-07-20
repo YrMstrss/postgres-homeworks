6e3af690dfdb07b6b9d72c49433cba57bb535dd6
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-07-20 22:11:09 +0300

    add create table orders

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -13,3 +13,11 @@ CREATE TABLE employees (
     birth_date varchar(100),
     notes text
 )
+
+CREATE TABLE orders (
+    order_id int PRIMARY KEY,
+    customer_id int REFERENCES customers (customer_id) NOT NULL,
+    employee_id int REFERENCES employees (employee_id) NOT NULL,
+    order_date varchar(100),
+    ship_city varchar(100)
+)
