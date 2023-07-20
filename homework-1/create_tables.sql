a46dd1d3ee79dd12a8b65fb8d73e869ada513bb5
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-07-20 22:09:07 +0300

    add create table employees

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,7 +1,15 @@
 -- SQL-команды для создания таблиц
-CREATE TABLE  customers
-(
+CREATE TABLE  customers (
     customer_id  int PRIMARY KEY,
     company_name varchar(100),
     contact_name varchar(100)
+)
+
+CREATE TABLE employees (
+    employee_id int PRIMARY KEY,
+    first_name varchar(100),
+    last_name varchar(100),
+    title varchar(100),
+    birth_date varchar(100),
+    notes text
 )
