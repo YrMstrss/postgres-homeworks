f6caaba03ce5a5433d2ef53aeff5d51d14380c0f
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-07-24 19:27:11 +0300

    add functional adding customers data into database

--- homework-1/main.py
+++ homework-1/main.py
@@ -5,10 +5,13 @@ import csv
 
 customers_way = os.path.join(os.path.dirname(__file__), 'north_data', 'customers_data.csv')
 
-with open(customers_way, newline='') as csvfile:
-    reader = csv.DictReader(csvfile)
+with psycopg2.connect(host='localhost', database='north', user='postgres', password='qwaszxL1') as conn:
+    with conn.cursor() as cur:
+        with open(customers_way, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
 
-    for row in reader:
-        customer_id, company_name, contact_name = row['customer_id'], row['company_name'], row['contact_name']
-        print(customer_id, company_name, contact_name)
+            for row in reader:
+                customer_id, company_name, contact_name = row['customer_id'], row['company_name'], row['contact_name']
+                cur.execute('INSERT INTO customers VALUES (%s, %s, %s)', (customer_id, company_name, contact_name))
 
+conn.close()
