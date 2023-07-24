d8df0b9a1522f9e7bc634e6f15c6d73037b85cd5
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-07-24 19:18:57 +0300

    get data from customers_data.csv

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,14 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import os
+import csv
+
+
+customers_way = os.path.join(os.path.dirname(__file__), 'north_data', 'customers_data.csv')
+
+with open(customers_way, newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+
+    for row in reader:
+        customer_id, company_name, contact_name = row['customer_id'], row['company_name'], row['contact_name']
+        print(customer_id, company_name, contact_name)
+
