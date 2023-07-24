8b4fe63894965b391b97d090ed48ae5062e3961e
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-07-24 19:11:14 +0300

    error correction

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,6 +1,6 @@
 -- SQL-команды для создания таблиц
 CREATE TABLE  customers (
-    customer_id  int PRIMARY KEY,
+    customer_id  varchar(10) PRIMARY KEY,
     company_name varchar(100),
     contact_name varchar(100)
 )
@@ -10,14 +10,14 @@ CREATE TABLE employees (
     first_name varchar(100),
     last_name varchar(100),
     title varchar(100),
-    birth_date varchar(100),
+    birth_date date,
     notes text
 )
 
 CREATE TABLE orders (
     order_id int PRIMARY KEY,
-    customer_id int REFERENCES customers (customer_id) NOT NULL,
+    customer_id varchar(10) REFERENCES customers (customer_id) NOT NULL,
     employee_id int REFERENCES employees (employee_id) NOT NULL,
-    order_date varchar(100),
+    order_date date,
     ship_city varchar(100)
 )
